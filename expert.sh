2ff2b1e6d46fb0957feab7fddaaf257a6af829db
OctMon <octmon@qq.com>
2018-12-15 16:16:43 +0800

    Format

--- expert.sh
+++ expert.sh
@@ -7,8 +7,8 @@ project=`find . -name *.xcodeproj | awk -F "[/.]" '{print $(NF-1)}'`
 ##========================根据实际情况修改(一般不用修改)========================
 ## true or false
 workspace="true"
-## TARGETS对应的Scheme 默认项目名称
-scheme=$project
+## 打包方式对应的TARGETS 默认项目名称
+scheme=${project}
 ## TARGETS对应的plist 不需要加后缀 默认Info.plist
 info="Info"
 ## Release or Debug 默认Release
@@ -27,15 +27,15 @@ read -p "更新日志(同一条日志不要有空格)写好后按回车继续 " 
 count=1
 history=""
 for line in $(cat update_log)
-do 
-    history+="[${count}] ${line}.  "
-    count=$[$count+1]
+do
+history+="[${count}] ${line}.  "
+count=$[${count}+1]
 done
 
 rm -rf update_log
-update_log="$history    *https://github.com/OctMon/EasyExpertApp 自动构建*"
+update_log=${history}
 echo "更新日志:"
-echo $update_log
+echo ${update_log}
 ##==========================================================================
 
 ##================================选择打包方式================================
@@ -45,47 +45,47 @@ echo "\033[32;1m2. ad-hoc      \033[0m"
 echo "\033[33;1m3. app-store   \033[0m"
 echo "\033[34;1m4. enterprise  \033[0m"
 read parameter
-method=$parameter
+method=${parameter}
 path_export_options=""
-if [ -n $method ]
+if [ -n ${method} ]
 then
-    if [ $method = "1" ] ; then
-    path_export_options="development.plist"
-    elif [ $method = "2" ] ; then
-    path_export_options="ad-hoc.plist"
-    elif [ $method = "3" ] ; then
-    path_export_options="app-store.plist"
-    elif [ $method = "4" ] ; then
-    path_export_options="enterprise.plist"
-    else
-    echo "参数错误"
-    exit 1
-    fi
+if [ ${method} = "1" ] ; then
+path_export_options="development.plist"
+elif [ ${method} = "2" ] ; then
+path_export_options="ad-hoc.plist"
+elif [ ${method} = "3" ] ; then
+path_export_options="app-store.plist"
+elif [ ${method} = "4" ] ; then
+path_export_options="enterprise.plist"
+else
+echo "参数错误"
+exit 1
+fi
 fi
 ##==========================================================================
 
 path_build=build
-path_archive="$path_build/$scheme.xcarchive"
- 
+path_archive="${path_build}/${scheme}.xcarchive"
+
 ##===================================归档====================================
 if $is_workspace ; then
 xcodebuild clean -workspace ${project}.xcworkspace \
-                 -scheme ${scheme} \
-                 -configuration ${configuration}
+-scheme ${scheme} \
+-configuration ${configuration}
 
 xcodebuild archive -workspace ${project}.xcworkspace \
-                   -scheme ${scheme} \
-                   -configuration ${configuration} \
-                   -archivePath ${path_archive}
+-scheme ${scheme} \
+-configuration ${configuration} \
+-archivePath ${path_archive}
 else
 xcodebuild clean -project ${project}.xcodeproj \
-                 -scheme ${scheme} \
-                 -configuration ${configuration}
+-scheme ${scheme} \
+-configuration ${configuration}
 
 xcodebuild archive -project ${project}.xcodeproj \
-                   -scheme ${scheme} \
-                   -configuration ${configuration} \
-                   -archivePath ${path_archive}
+-scheme ${scheme} \
+-configuration ${configuration} \
+-archivePath ${path_archive}
 fi
 ##==========================================================================
 
@@ -96,15 +96,15 @@ path_package="${path_build}/${scheme}_${bundle_build}"
 
 ##==================================导出ipa==================================
 xcodebuild  -exportArchive \
-            -archivePath ${path_archive} \
-            -exportPath ${path_package} \
-            -exportOptionsPlist ${path_export_options}
+-archivePath ${path_archive} \
+-exportPath ${path_package} \
+-exportOptionsPlist ${path_export_options}
 ##==========================================================================
 
 mv $path_archive $path_package
 
 #上传到pgyer
-curl -F "file=@${path_package}/${scheme}.ipa" -F "_api_key=${pgyer_api_key}" -F "buildUpdateDescription=$update_log  ($bundle_build)" https://www.pgyer.com/apiv2/app/upload
+curl -F "file=@${path_package}/${scheme}.ipa" -F "_api_key=${pgyer_api_key}" -F "buildUpdateDescription=${update_log}      *https://github.com/OctMon/EasyExpertApp build(${bundle_build})*" https://www.pgyer.com/apiv2/app/upload
 
 echo
 echo "** 上传完成 用时: ${SECONDS}s **"
